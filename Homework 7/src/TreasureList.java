8a7edbd2adf4765884374bc4e18d03af624118dc
Chiara <zizzachi@grinnell.edu>
2016-11-10 11:53:57 -0600

    Add files via upload

--- Homework 7/src/TreasureList.java
+++ Homework 7/src/TreasureList.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class TreasureList {
@@ -8,25 +9,49 @@ public class TreasureList {
 	
 	public TreasureList() {
 		this.arr = new String[4];
-		this.treasure = new File("Homework 7/loot-generator-data/small/TreasureClassEx.txt");
+		this.treasure = new File("/Users/chiarazizza/Documents/workspace/Homework 7/loot-generator-data/small/TreasureClassEx.txt");
 	}
 	
 	public void populate() throws FileNotFoundException {
 		Scanner scan = new Scanner(treasure);
 		String line = scan.nextLine();
-		for(int i = 0; i < 4; i++) {   /////MAYBE SWAP ORDER?
-			arr[i] = findNextWord(line);
-		}
+//		for(int i = 0; i < 4; i++) {   /////MAYBE SWAP ORDER?
+//			arr[i] = findNextWord(line, 0);
+//		}
+		findNextWord(line, 0);
 		scan.close();
 	}
 	
-	public String findNextWord(String str) {
-		if(str.contains("\t")) {
-			int tabIndex = str.indexOf("\t");
-			findNextWord(str.substring(tabIndex + 1, str.length()));
-			return str.substring(0, tabIndex);
+//	public String findNextWord(String str, String TC, String, item1, ) {
+//		if(str.contains("\t")) {
+//			int tabIndex = str.indexOf("\t");
+//			System.out.println(tabIndex);
+//			findNextWord(str.substring(tabIndex + 1, str.length()));
+//			
+//			System.out.println(str.substring(tabIndex + 1, str.length()));
+//			return str.substring(0, tabIndex);
+//		} else {
+//			return str;
+//		}
+//	}
+	
+	public void findNextWord(String str, int i) {
+		if(!str.contains("\t")) {
+			this.arr[i] = str;
 		} else {
-			return str;
+			int tabIndex = str.indexOf("\t");
+			System.out.println(tabIndex);
+			
+			this.arr[i] = str.substring(0, tabIndex);
+			findNextWord(str.substring(tabIndex + 1, str.length()), i++);
+			
+			System.out.println(str.substring(tabIndex + 1, str.length()));
 		}
+	}
+	
+	public static void main(String[] args) throws FileNotFoundException {
+		TreasureList list = new TreasureList();
+		list.populate();
+		System.out.println(Arrays.toString(list.arr));
 	}
 }
