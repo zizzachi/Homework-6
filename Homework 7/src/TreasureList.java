d5769ae54d263516f318a4e426af41d4413c0a56
Maddie Kirwin <kirwinma@grinnell.edu>
2016-11-10 15:28:03 -0600

    took out comments

--- Homework 7/src/TreasureList.java
+++ Homework 7/src/TreasureList.java
@@ -6,50 +6,30 @@ import java.util.Scanner;
 public class TreasureList {
 	private String[] arr;
 	File treasure;
-	
+
 	public TreasureList() {
 		this.arr = new String[4];
-		this.treasure = new File("/Users/chiarazizza/Documents/workspace/Homework 7/loot-generator-data/small/TreasureClassEx.txt");
+		this.treasure = new File("/Users/user1/workspace/207-homework-07/"
+				+ "loot-generator-data/small/TreasureClassEx.txt");
 	}
-	
+
 	public void populate() throws FileNotFoundException {
 		Scanner scan = new Scanner(treasure);
 		String line = scan.nextLine();
-//		for(int i = 0; i < 4; i++) {   /////MAYBE SWAP ORDER?
-//			arr[i] = findNextWord(line, 0);
-//		}
-		findNextWord(line, 0);
+		populateHelper(line, 0);
 		scan.close();
 	}
-	
-//	public String findNextWord(String str, String TC, String, item1, ) {
-//		if(str.contains("\t")) {
-//			int tabIndex = str.indexOf("\t");
-//			System.out.println(tabIndex);
-//			findNextWord(str.substring(tabIndex + 1, str.length()));
-//			
-//			System.out.println(str.substring(tabIndex + 1, str.length()));
-//			return str.substring(0, tabIndex);
-//		} else {
-//			return str;
-//		}
-//	}
-	
-	public void findNextWord(String str, int i) {
-		if(!str.contains("\t")) {
+
+	public void populateHelper(String str, int i) {
+		if (!str.contains("\t")) {
 			this.arr[i] = str;
-			System.out.println(str);
 		} else {
 			int tabIndex = str.indexOf("\t");
-			System.out.println(tabIndex);
-			
 			this.arr[i] = str.substring(0, tabIndex);
-			findNextWord(str.substring(tabIndex + 1, str.length()), i + 1);
-			
-			System.out.println(str.substring(0, tabIndex));
+			populateHelper(str.substring(tabIndex + 1, str.length()), i + 1);
 		}
 	}
-	
+
 	public static void main(String[] args) throws FileNotFoundException {
 		TreasureList list = new TreasureList();
 		list.populate();
